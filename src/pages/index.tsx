298c2f2f97bf225986e8d71e4a958c993ea08002
mineTomek <lukom.tomek@gmail.com>
2023-03-22 19:13:52 +0100

    Sort and clean imports and remove inter font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import React, { useState, useEffect } from 'react'
-import Text from '../components/Text'
+import styles from '@/styles/Home.module.css'
 import PostCard from '../components/PostCard'
+import Text from '../components/Text'
+// import Logo from "../../public/logo.svg"
+import Image from 'next/image'
+import Head from 'next/head'
+//'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [userLanguage, setUserLanguage] = useState('en-US');
