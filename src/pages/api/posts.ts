a11f1b9ae93dc7e7530e8ce9e10cc373b9d86439
mineTomek <lukom.tomek@gmail.com>
2023-03-22 19:08:42 +0100

    Rename post to posts and add req.query to posts.ts

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -9,5 +9,5 @@ export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    res.status(200).json({ data: "All the posts" })
+    res.status(200).json({ data: "All the posts : " + req.query["test"] })
 }
