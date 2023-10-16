d7a68948a10fc2530248175c354c242571ccd83c
mineTomek <lukom.tomek@gmail.com>
2023-10-16 17:43:23 +0200

    Always return a dummy response to check for 504

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -6,27 +6,37 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    const db = await getDb()
+  res.status(200).json({
+    posts: [
+      {
+        _id: 'id',
+        title: { pl: 'Tytuł', en: 'Title' },
+        content: { pl: 'Zawartość', en: 'Content' },
+      },
+    ],
+  })
 
-    const collection = db.collection('posts')
+  // try {
+  //   const db = await getDb()
 
-    let query = {}
+  //   const collection = db.collection('posts')
 
-    const { q } = req.query
+  //   let query = {}
 
-    if (q) {
-      try {
-        query = JSON.parse(q.toString())
-      } catch (error) {
-        throw new Error('Invalid query parameter')
-      }
-    }
+  //   const { q } = req.query
 
-    const posts = await collection.find(query).toArray()
+  //   if (q) {
+  //     try {
+  //       query = JSON.parse(q.toString())
+  //     } catch (error) {
+  //       throw new Error('Invalid query parameter')
+  //     }
+  //   }
 
-    res.status(200).json(posts)
-  } catch (error) {
-    createError((error as Error).message, res, 500, error as Error)
-  }
+  //   const posts = await collection.find(query).toArray()
+
+  //   res.status(200).json(posts)
+  // } catch (error) {
+  //   createError((error as Error).message, res, 500, error as Error)
+  // }
 }
