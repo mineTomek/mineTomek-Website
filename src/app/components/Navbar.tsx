ad0caade4cb7a6ca4521cfaca8b70fc42a2c5473
mineTomek <lukom.tomek@gmail.com>
2023-10-18 20:56:51 +0200

    Improve the navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,12 +5,15 @@ const kdamThmorPro = Kdam_Thmor_Pro({ subsets: ['latin'], weight: '400' })
 
 export default function Navbar() {
   return (
-    <div className='flex flex-row justify-between items-center px-4 pt-4 gap-2'>
+    <div
+      className={`fixed top-0 .-inset-x-16 inset-x-0 flex flex-row justify-evenly items-center gap-2 h-16 bg-gradient-to-b from-white to-slate-100  ${kdamThmorPro.className}`}
+    >
       <Logo
         width={32}
         height={32}
       />
-      <h1 className={`text-xl ${kdamThmorPro.className}`}>mineTomek</h1>
+      <h1 className='text-xl'>mineTomek</h1>
+      <div>Menu</div>
     </div>
   )
 }
