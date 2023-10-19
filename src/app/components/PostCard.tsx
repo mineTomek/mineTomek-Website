3e3085143786336aa18a9bb554ee3966a43e9d04
mineTomek <lukom.tomek@gmail.com>
2023-10-19 16:54:42 +0200

    Change post card from box to flex

--- src/app/components/PostCard.tsx
+++ src/app/components/PostCard.tsx
@@ -15,7 +15,7 @@ const PostCard: FC<Props> = props => {
   return (
     <Link
       href={props.link}
-      className='p-4 bg-gradient-to-b from-slate-50 to-slate-100 block rounded-xl'
+      className='p-4 bg-gradient-to-b from-slate-50 to-slate-100 rounded-xl flex flex-col'
       target='_blank'
       rel='noopener noreferrer'
     >
