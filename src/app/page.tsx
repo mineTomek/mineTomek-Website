347a15b008bbbfb13f07c0f0addb5a81c3435ec2
mineTomek <lukom.tomek@gmail.com>
2023-10-20 19:47:01 +0200

    Load categories and display them in a row

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Text from './components/Text'
 import useSWR from 'swr'
 import Post from './types/Post'
 import PostCard from './components/PostCard'
+import Category from './types/Category'
 
 const fetcher = (url: string) => fetch(url).then(res => res.json())
 
@@ -16,6 +17,12 @@ export default function Home() {
     isLoading: isLoadingPosts,
     error: postsError,
   } = useSWR<Post[]>('/api/posts', fetcher)
+
+  const {
+    data: categories,
+    isLoading: isLoadingCategories,
+    error: categoriesError,
+  } = useSWR<Category[]>('/api/categories', fetcher)
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -31,6 +38,23 @@ export default function Home() {
           lang={userLanguage}
         />
       </h2>
+
+      {isLoadingCategories && <p>Loading categories...</p>}
+      {categoriesError && (
+        <p>Categories loading error: {JSON.stringify(categoriesError)}</p>
+      )}
+
+      {categories && !isLoadingCategories && !categoriesError && (
+        <div className='flex flex-row gap-x-3'>
+          {categories!.map((category, i) => (
+            <span key={category._id.toString()} className='bg-slate-100 p-1 px-3 rounded-md'>{category.icon} {category.name}</span>
+          ))}
+        </div>
+      )}
+
+      {!categories && !isLoadingCategories && !categoriesError && (
+        <p>Posts data is not available!</p>
+      )}
 
       {isLoadingPosts && <p>Loading posts...</p>}
       {postsError && <p>Post loading error: {JSON.stringify(postsError)}</p>}
