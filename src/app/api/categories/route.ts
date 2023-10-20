5708d238ac0da87482dfaa0cce7299d0d2057c7c
mineTomek <lukom.tomek@gmail.com>
2023-10-20 19:34:28 +0200

    Add API route for getting categories

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,33 @@
+import createError from '@/app/api/helpers/createError'
+import getDb from '@/app/api/helpers/db'
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  try {
+    const db = await getDb()
+
+    const collection = db.collection('categories')
+
+    let query = {}
+
+    const q = request.nextUrl.searchParams.get('q')
+
+    if (q) {
+      try {
+        query = JSON.parse(q.toString())
+      } catch (error) {
+        return createError(
+          `Invalid query parameter: '${q}'`,
+          400,
+          error as Error
+        )
+      }
+    }
+
+    const posts = await collection.find(query).toArray()
+
+    return Response.json(posts)
+  } catch (error) {
+    return createError((error as Error).message, 500, error as Error)
+  }
+}
