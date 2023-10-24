fb58552eb719f8d6a4d286fe7b98de7a4376df63
mineTomek <lukom.tomek@gmail.com>
2023-10-24 16:48:27 +0200

    Move metadata to one place

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Head from 'next/head'
 import Navbar from './components/Navbar'
 import { getContentFont } from './fonts'
 
 export const metadata: Metadata = {
   title: 'mineTomek',
   description: "mineTomek's official website",
+  icons: { icon: '/favicon.png' },
+  themeColor: '#4e9c76',
+  viewport: 'width=device-width, initial-scale=1',
 }
 
 export default function RootLayout({
@@ -16,32 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <Head>
-        <title>mineTomek</title>
-        <meta
-          name='description'
-          content="mineTomek's Website"
-        />
-        <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1'
-        />
-        <link
-          rel='icon'
-          type='image/svg+xml'
-          href='/favicon.svg'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          href='/favicon.png'
-        />
-        <meta
-          name='theme-color'
-          content='#4E9C76'
-        />
-      </Head>
-      <body className={`bg-background ${getContentFont().className} text-text-800`}>
+      <body
+        className={`bg-background ${getContentFont().className} text-text-800`}
+      >
         <Navbar />
         <div className='pt-16'>{children}</div>
       </body>
