a4227f31d0c4eca2b44fcbb07aa1b5bb65a4878c
mineTomek <lukom.tomek@gmail.com>
2023-10-24 20:58:34 +0200

    Add option to have custom css classes on the logo

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 
-export default function Logo(props: { width?: number; height?: number }) {
+export default function Logo(props: {
+  width?: number
+  height?: number
+  className?: string
+}) {
   return (
     <Image
       src={'/favicon.svg'}
       alt='logo'
       width={props.width ?? 48}
       height={props.height ?? 48}
+      className={props.className}
     />
   )
 }
