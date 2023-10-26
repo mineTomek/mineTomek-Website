2ca3897fe8bfb5b8821fec55bf82b8b5f71cb64a
mineTomek <lukom.tomek@gmail.com>
2023-10-26 17:45:27 +0200

    Change navbar to justify between and add x padding

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 export default function Navbar() {
   return (
     <div
-      className={`.-inset-x-16 fixed inset-x-0 top-0 flex h-16 flex-row items-center justify-evenly gap-2 bg-gradient-to-b from-white to-slate-100  ${
+      className={`.-inset-x-16 fixed inset-x-0 top-0 flex h-16 flex-row items-center justify-between gap-2 bg-gradient-to-b from-white to-slate-100 px-4 ${
         getTitleFont().className
       }`}
     >
