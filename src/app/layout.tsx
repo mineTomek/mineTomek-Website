12224901344d4f1f42dc89974406af322f0d3294
mineTomek <lukom.tomek@gmail.com>
2023-11-01 10:50:50 +0100

    Make the whole website by default darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className='dark'>
       <body
         className={`bg-background ${getContentFont().className} text-text-800`}
       >
