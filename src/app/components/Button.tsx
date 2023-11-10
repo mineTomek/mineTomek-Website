dacbad52c7477aed2ae04c579c084d6b93d5c5f9
mineTomek <lukom.tomek@gmail.com>
2023-11-10 17:56:42 +0100

    Order the classes

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,7 +1,7 @@
 export default function Button(props: { text: string; disabled?: boolean }) {
   return (
     <button
-      className='w-fit cursor-pointer disabled:cursor-default disabled:opacity-50 rounded-md border bg-primary-100 p-2 [box-shadow:1px_-4px_3px_0_#00000070_inset] dark:border-primary-950 dark:bg-primary-800'
+      className='w-fit cursor-pointer rounded-md border bg-primary-100 p-2 [box-shadow:1px_-4px_3px_0_#00000070_inset] disabled:cursor-default disabled:opacity-50 dark:border-primary-950 dark:bg-primary-800'
       disabled={props.disabled ?? false}
     >
       {props.text}
