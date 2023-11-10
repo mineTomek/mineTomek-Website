779cef926fe159694a5e42ce72c078e7e6c0445b
mineTomek <lukom.tomek@gmail.com>
2023-11-10 17:49:59 +0100

    Make navbar always on top

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,6 +1,9 @@
-export default function Button(props: {text: string}) {
+export default function Button(props: { text: string; disabled?: boolean }) {
   return (
     <button className='w-fit cursor-pointer rounded-md border dark:border-primary-950 bg-primary-100 dark:bg-primary-800 p-2 [box-shadow:1px_-4px_3px_0_#00000070_inset]'>
+      className='w-fit cursor-pointer disabled:cursor-default disabled:opacity-50 rounded-md border bg-primary-100 p-2 [box-shadow:1px_-4px_3px_0_#00000070_inset] dark:border-primary-950 dark:bg-primary-800'
+      disabled={props.disabled ?? false}
+    >
       {props.text}
     </button>
   )
