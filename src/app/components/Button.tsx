9ee7597a39d5efc3fdbbb636e45d9b8968737aed
mineTomek <lukom.tomek@gmail.com>
2023-11-13 22:12:31 +0100

    Add an option to add a classname to the button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -4,13 +4,17 @@ import { useRouter } from 'next/navigation'
 export default function Button(props: {
   text: string
   disabled?: boolean
+  className?: string
   clickAction?: (router: AppRouterInstance) => void
 }) {
   const router = useRouter()
 
   return (
     <button
-      className='w-fit cursor-pointer rounded-md border bg-primary-100 p-2 [box-shadow:1px_-4px_3px_0_#00000070_inset] disabled:cursor-default disabled:opacity-50 dark:border-primary-950 dark:bg-primary-800'
+      className={
+        'w-fit cursor-pointer rounded-md border bg-primary-100 p-2 [box-shadow:1px_-4px_3px_0_#00000070_inset] disabled:cursor-default disabled:opacity-50 dark:border-primary-950 dark:bg-primary-800 ' +
+          props.className ?? ''
+      }
       disabled={props.disabled ?? false}
       onClick={() => {
         if (props.clickAction) {
