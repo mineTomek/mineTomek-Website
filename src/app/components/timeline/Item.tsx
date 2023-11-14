243c2af66dd1bfa31afb771feab364c73c1b6cba
mineTomek <lukom.tomek@gmail.com>
2023-11-14 21:03:13 +0100

    Add some animations to the timeline page

--- src/app/components/timeline/Item.tsx
+++ src/app/components/timeline/Item.tsx
@@ -2,10 +2,17 @@ import TimelineItem from '@/app/types/timeline/TimelineItem'
 import Image from 'next/image'
 import Button from '../Button'
 import Markdown from 'react-markdown'
+import { motion } from 'framer-motion'
+import { useRef } from 'react'
 
 export default function Item(props: { item: TimelineItem; index: number }) {
   return (
-    <div className='flex flex-col gap-4 p-8 before:ml-1 md:relative md:w-1/2 md:before:absolute md:before:bottom-0 md:before:top-0 md:before:w-2 md:before:bg-zinc-400 md:before:first:[border-radius:0.375rem_0.375rem_0_0] md:before:last:[border-radius:0_0_0.375rem_0.375rem] md:odd:before:left-full md:even:left-1/2 md:even:before:-left-0'>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true, margin: '-10%' }}
+      className='flex flex-col gap-4 p-8 before:ml-1 md:relative md:w-1/2 md:before:absolute md:before:bottom-0 md:before:top-0 md:before:w-2 md:before:bg-zinc-400 md:before:first:[border-radius:0.375rem_0.375rem_0_0] md:before:last:[border-radius:0_0_0.375rem_0.375rem] md:odd:before:left-full md:even:left-1/2 md:even:before:-left-0'
+    >
       <div className='flex justify-between text-xl'>
         <p className='bold'>{props.item.title}</p>
         <p className='text-zinc-500'>
@@ -32,13 +39,19 @@ export default function Item(props: { item: TimelineItem; index: number }) {
         })}
       </div>
 
-      <Image
-        src={props.item.imageSrc}
-        alt={`${props.item.title} image`}
-        width={1920}
-        height={1080}
-        className='aspect-video w-full rounded-lg object-cover'
-      />
-    </div>
+      <motion.div
+        initial={{ scaleX: 0 }}
+        whileInView={{ scaleX: 1 }}
+        viewport={{ once: true }}
+      >
+        <Image
+          src={props.item.imageSrc}
+          alt={`${props.item.title} image`}
+          width={1920}
+          height={1080}
+          className='aspect-video w-full rounded-lg object-cover'
+        />
+      </motion.div>
+    </motion.div>
   )
 }
