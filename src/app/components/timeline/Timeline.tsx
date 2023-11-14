bfd12cfc46aa9dd836925981b2e16005a01a4347
mineTomek <lukom.tomek@gmail.com>
2023-11-14 10:44:33 +0100

    Simplify .map()

--- src/app/components/timeline/Timeline.tsx
+++ src/app/components/timeline/Timeline.tsx
@@ -4,11 +4,13 @@ import Item from './Item'
 export default function Timeline(props: { items: TimelineItem[] }) {
   return (
     <div className=''>
-      {props.items.map((item, i) => {
-        return (
-          <Item item={item} index={i} key={i}/>
-        )
-      })}
+      {props.items.map((item, i) => (
+        <Item
+          item={item}
+          index={i}
+          key={i}
+        />
+      ))}
     </div>
   )
 }
