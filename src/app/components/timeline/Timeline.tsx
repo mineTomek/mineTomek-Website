4e3775a371d7d0fe6e29f09b82cdf20dad4de602
mineTomek <lukom.tomek@gmail.com>
2023-11-15 16:54:14 +0100

    Remove dead code

--- src/app/components/timeline/Timeline.tsx
+++ src/app/components/timeline/Timeline.tsx
@@ -3,7 +3,7 @@ import Item from './Item'
 
 export default function Timeline(props: { items: TimelineItem[] }) {
   return (
-    <div className=''>
+    <div>
       {props.items.map((item, i) => (
         <Item
           item={item}
