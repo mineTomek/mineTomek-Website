c5cd262a1741e7d1b07d13f8932a4e67e8c5e1fd
mineTomek <lukom.tomek@gmail.com>
2023-11-15 16:53:54 +0100

    Sort items on timeline by date

--- src/app/api/timeline/items/route.ts
+++ src/app/api/timeline/items/route.ts
@@ -24,7 +24,7 @@ export async function GET(request: NextRequest) {
       }
     }
 
-    const items = await collection.find(query).toArray()
+    const items = await collection.find(query).sort({ date: 1 }).toArray()
 
     return Response.json(items)
   } catch (error) {
